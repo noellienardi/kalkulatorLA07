21889de0afc71dc85fc0b013d3767d97bad9cc1c
noellienardi <159357noel@gmail.com>
2021-12-22 02:33:09 -0500

    menambahkan fungsi pengurangan dan pengalian

--- calc.py
+++ calc.py
@@ -1,8 +1,18 @@
 def plus (a,b):
-    total = a+b
-    return total
+    return (a+b)
+
+def minus (a,b):
+    return (a-b)
+
+def multi (a,b):
+    return (a*b)
 
 def main():
+    print ("Penambahan")
     print (plus (10,5))
+    print ("Pengurangan")
+    print (minus(10,5))
+    print("Perkalian")
+    print (multi(10,5))
 
 main()
